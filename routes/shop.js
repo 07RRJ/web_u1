f4b535f290a023bf4cc71151b5a4300c2a183d88
07RRJ <rasmusrudko07@gmail.com>
2025-09-08 12:19:57 +0200

    2

--- routes/shop.js
+++ routes/shop.js
@@ -6,26 +6,19 @@ const path = require('path');
 const dataPath = path.join(__dirname, "..", 'data.json');
 const jsonData = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
 router.get('/', (req, res, next) => {
-  res.render('index', { names: jsonData.names});
-});
-
-routs.get("/", (req, res, next) => {
-  res.render('page', {}) 
+  res.render('landingpg', { names: jsonData.names});
 });
 
 router.get('/', function (req, res, next) {
   res.render('landingpg', {}) 
-}
-);
+});
 
 router.get('/page', function (req, res, next) {
   res.render('page', {}) 
-}
-);
+});
 
 router.get('/page1', function (req, res, next) {
   res.render('page1', {}) 
-}
-);
+});
 
 module.exports = router;
